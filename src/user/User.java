19bf77da8fa92e49762f3920062c3280d95c5dd8
deuk94 <knli0025@gmail.com>
2024-07-31 23:48:54 +0900

    패키지 변경

--- src/user/User.java
+++ src/user/User.java
@@ -2,7 +2,54 @@ package src.user;
 
 public class User {
 
-    public static void main(String[] args) {
-        System.out.println("hello");
+    private String userId;
+    private String userPw;
+    private String userName;
+    private String userGender;
+    private String userPhone;
+    private String userBirth;
+    private String userEmail;
+    private String userStatus;
+
+    public User(String userId, String userPw, String userName, String userGender,
+                String userPhone, String userBirth, String userEmail) {
+        this.userId = userId;
+        this.userPw = userPw;
+        this.userName = userName;
+        this.userGender = userGender;
+        this.userPhone = userPhone;
+        this.userBirth = userBirth;
+        this.userEmail = userEmail;
+    }
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserPw() {
+        return userPw;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserGender() {
+        return userGender;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public String getUserBirth() {
+        return userBirth;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getUserStatus() {
+        return userStatus;
     }
 }
---
+++ src/user/UserCreate.java
@@ -0,0 +1,10 @@
+package src.user;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import src.database.Database;
+import src.user.User;
+
+public class UserCreate {
+}
