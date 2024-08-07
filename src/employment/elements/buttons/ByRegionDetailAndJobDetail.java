b3569f84a7ca644558c3ce1527ef9044aca33843
mhd <mhd329@gmail.com>
2024-08-08 02:39:28 +0900

    현동 작업 많이 수정

--- src/employment/elements/buttons/ByRegionDetailAndJobDetail.java
+++ src/employment/elements/buttons/ByRegionDetailAndJobDetail.java
@@ -3,6 +3,6 @@ package src.employment.elements.buttons;
 public class ByRegionDetailAndJobDetail extends Button {
     @Override
     public void printButton() {
-        System.out.print("조합 검색\t");
+        System.out.print("종합 검색\t");
     }
 }
--- src/employment/recordDAO/employmentBoardScrap/Scraper.java
+++ src/employment/recordDAO/employmentBoardScrap/Scraper.java
@@ -1,5 +1,6 @@
 package src.employment.recordDAO.employmentBoardScrap;
 
+
 import src.employment.board.BoardDTO;
 import src.employment.recordDAO.employmentBoard.ReadDAO;
 import src.user.UserMain;
@@ -9,27 +10,6 @@ import java.util.Scanner;
 
 
 public class Scraper {
-    public void selectScrapTarget(Scanner sc) {
-        String userInput = "";
-        int userInputNumber = 0;
-        boolean breakFlag = false;
-        while (!breakFlag) {
-            System.out.print("스크랩 할 게시물 번호를 입력하세요 (Q. 돌아가기): ");
-            userInput = sc.nextLine();
-            if (userInput.equals("Q")) {
-                breakFlag = true;
-            } else {
-                try {
-                    userInputNumber = Integer.parseInt(userInput);
-                    breakFlag = true;
-                    scrapToMyPage(userInputNumber);
-                } catch (NumberFormatException e) {
-                    System.out.println("다시 입력해주세요.");
-                }
-            }
-        }
-    }
-
     public void selectDeleteTarget(Scanner sc) {
         String userInput = "";
         int userInputNumber = 0;
@@ -51,24 +31,6 @@ public class Scraper {
         }
     }
 
-    public void scrapToMyPage(int userInputNumber) {
-        ReadDAO readDAO = new ReadDAO();
-        Response<BoardDTO> readResponse = readDAO.read(userInputNumber);
-        if (readResponse.isSuccess()) {
-            BoardDTO boardDTO = readResponse.getData();
-            int bid = boardDTO.getEmploymentBoardId();
-            CreateScrapDAO createScrapDAO = new CreateScrapDAO();
-            Response<String> createResponse = createScrapDAO.create(UserMain.id, bid);
-            if (createResponse.isSuccess()) {
-                System.out.println("성공적으로 스크랩 되었습니다.");
-            } else {
-                System.out.println("스크랩에 실패하였습니다.");
-            }
-        } else {
-            System.out.println("게시글 불러오기 실패.");
-        }
-    }
-
     public void deleteToMyPage(int userInputNumber) {
         ReadDAO readDAO = new ReadDAO();
         Response<BoardDTO> readResponse = readDAO.read(userInputNumber);
@@ -78,12 +40,12 @@ public class Scraper {
             DeleteScrapDAO deleteScrapDAO = new DeleteScrapDAO();
             Response<String> deleteResponse = deleteScrapDAO.delete(UserMain.id, bid);
             if (deleteResponse.isSuccess()) {
-                System.out.println("해제 되었습니다.");
+                System.out.println("스크랩 해제 되었습니다.");
             } else {
-                System.out.println("해제에 실패하였습니다.");
+                System.out.println("스크랩 해제에 실패하였습니다.");
             }
         } else {
-            System.out.println("게시글 불러오기 실패.");
+            System.out.println("채용공고가 더 이상 존재하지 않습니다.");
         }
     }
 }
